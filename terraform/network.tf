ce38d5f14a596c215f341f156f114bde6c604a1a
hippogriffin <40152338+hippogriffin@users.noreply.github.com>
2022-06-29 21:53:01 +0100

    update subnet reference

--- terraform/network.tf
+++ terraform/network.tf
@@ -3,23 +3,20 @@ resource "azurerm_virtual_network" "logic_app" {
   location            = azurerm_resource_group.logic_app.location
   resource_group_name = azurerm_resource_group.logic_app.name
   address_space       = ["10.0.0.0/16"]
+}
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-  }
+resource "azurerm_subnet" "logic_app" {
+  name                 = "logic-app-subnet"
+  resource_group_name  = azurerm_resource_group.logic_app.name
+  virtual_network_name = azurerm_virtual_network.logic_app.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_private_endpoint" "logic_app" {
   name                = "logic-app-endpoint"
   location            = azurerm_resource_group.logic_app.location
   resource_group_name = azurerm_resource_group.logic_app.name
-  subnet_id           = azurerm_virtual_network.logic_app.subnet[0].id
+  subnet_id           = azurerm_subnet.logic_app.id
 
   private_service_connection {
     name                              = "logic-app-psc"
