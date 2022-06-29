b385c3288e90e3ae931621ca70c883f514b84e04
hippogriffin <40152338+hippogriffin@users.noreply.github.com>
2022-06-29 22:12:06 +0100

    update storage account reference

--- terraform/storage_account.tf
+++ terraform/storage_account.tf
@@ -18,13 +18,13 @@ resource "azurerm_storage_container" "logic_app" {
 }
 
 resource "azurerm_role_assignment" "logic_app" {
-  scope              = azurerm_storage_account.logic_app.name
+  scope              = azurerm_storage_account.logic_app.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id       = azurerm_logic_app_workflow.logic_app.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "standard_logic_app" {
-  scope              = azurerm_storage_account.logic_app.name
+  scope              = azurerm_storage_account.logic_app.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id       = azurerm_logic_app_standard.logic_app.identity[0].principal_id
 }
