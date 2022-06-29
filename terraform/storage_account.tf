f9204adb80a1a68b995cb291dd548d1ff2540e55
hippogriffin <40152338+hippogriffin@users.noreply.github.com>
2022-06-29 23:05:00 +0100

    add myip resource

--- terraform/storage_account.tf
+++ terraform/storage_account.tf
@@ -6,11 +6,16 @@ resource "azurerm_storage_account" "logic_app" {
   account_replication_type = "LRS"
 }
 
+data "http" "myip" {
+  url = "http://ifconfig.me"
+}
+
 resource "azurerm_storage_account_network_rules" "logic_app" {
   storage_account_id = azurerm_storage_account.logic_app.id
 
   default_action             = "Deny"
   virtual_network_subnet_ids = [azurerm_subnet.logic_app.id]
+  ip_rules = [chomp(data.http.myip.body)]
 }
 
 resource "azurerm_storage_container" "logic_app" {
