bc03e6c7b3456b4c4ebd595df0f3f87811db8a20
hippogriffin <40152338+hippogriffin@users.noreply.github.com>
2022-06-29 22:47:21 +0100

    move network_rules

--- terraform/storage_account.tf
+++ terraform/storage_account.tf
@@ -6,8 +6,8 @@ resource "azurerm_storage_account" "logic_app" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_account_network_rules" "example" {
-  storage_account_id = azurerm_storage_account.example.id
+resource "azurerm_storage_account_network_rules" "logic_app" {
+  storage_account_id = azurerm_storage_account.logic_app.id
 
   default_action             = "Deny"
   virtual_network_subnet_ids = [azurerm_subnet.logic_app.id]
