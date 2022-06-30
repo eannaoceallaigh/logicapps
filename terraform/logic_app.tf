43de0583f1a9e53a39bbb29eb072cb3d63105be1
hippogriffin <40152338+hippogriffin@users.noreply.github.com>
2022-06-30 09:22:42 +0100

    Update logic_app.tf

--- terraform/logic_app.tf
+++ terraform/logic_app.tf
@@ -2,6 +2,9 @@ resource "azurerm_logic_app_workflow" "logic_app" {
   name                = "ek-logic-app"
   location            = azurerm_resource_group.logic_app.location
   resource_group_name = azurerm_resource_group.logic_app.name
+  workflow_parameters = {
+    keyVaultSecret = azurerm_key_vault_secret.logic_app.value
+  }
 
   identity {
     type = "SystemAssigned"
