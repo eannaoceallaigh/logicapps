7d091fa2aa3520b9fdf69cd742b905f312945cfb
hippogriffin <40152338+hippogriffin@users.noreply.github.com>
2022-06-30 11:42:10 +0100

    Update logic_app.tf

--- terraform/logic_app.tf
+++ terraform/logic_app.tf
@@ -8,14 +8,17 @@ resource "azurerm_logic_app_workflow" "logic_app" {
   }
 }
 
+locals {
+  parameters_content = {
+    keyVaultSecret = "https://${azurerm_key_vault.logic_app.name}.vault.azure.net/${azurerm_key_vault_secret.mySecret.name}"
+    workflows_logic_app_name = azurerm_logic_app_workflow.logic_app.name
+  }
+}
+
 resource "azurerm_template_deployment" "logicApp" {
   resource_group_name = azurerm_resource_group.logic_app.name
   deployment_mode     = "Incremental"
   name                = "logic-app-deployment"
-  parameters = {
-    workflows_logic_app_name = azurerm_logic_app_workflow.logic_app.name
-    location            = azurerm_resource_group.logic_app.location
-    keyVaultSecret      = "https://${azurerm_key_vault.logic_app.name}.vault.azure.net/${azurerm_key_vault_secret.mySecret.name}"
-  }
+  parameters_content  = jsonencode(local.parameters_content)
   template_body = data.local_file.logic_app.content
 }
