02bc202b179c7c417839ca4431a12d75bd80827f
hippogriffin <40152338+hippogriffin@users.noreply.github.com>
2022-06-30 12:30:18 +0100

    Update logic_app.tf

--- terraform/logic_app.tf
+++ terraform/logic_app.tf
@@ -8,12 +8,22 @@ resource "azurerm_logic_app_workflow" "logic_app" {
   }
 }
 
-# locals {
-#   parameters_content = {
-#     "keyVaultSecret" = "https://${azurerm_key_vault.logic_app.name}.vault.azure.net/${azurerm_key_vault_secret.mySecret.name}"
+locals {
+    keyVaultSecret = "https://${azurerm_key_vault.logic_app.name}.vault.azure.net/${azurerm_key_vault_secret.mySecret.name}"
 #     "workflows_logic_app_name" = azurerm_logic_app_workflow.logic_app.name
-#   }
-# }
+}
+
+
+resource "azurerm_template_deployment" "logic_app" {
+  resource_group_name = azurerm_resource_group.logic_app.name
+  deployment_mode     = "Incremental"
+  name                = "logic-app-deployment"
+  parameters = {
+    workflows_flow_name = azurerm_logic_app_workflow.logic_app.name
+    location            = azurerm_resource_group.logic_app.location
+  }
+  template_body = data.local_file.logic_app.content
+}
 
 # resource "azurerm_resource_group_template_deployment" "logicApp" {
 #   resource_group_name = azurerm_resource_group.logic_app.name
@@ -23,32 +33,32 @@ resource "azurerm_logic_app_workflow" "logic_app" {
 #   template_content = data.local_file.logic_app.content
 # }
 
-resource "azurerm_logic_app_trigger_http_request" "logic_app" {
-  name         = "http-trigger"
-  logic_app_id = azurerm_logic_app_workflow.logic_app.id
+# resource "azurerm_logic_app_trigger_http_request" "logic_app" {
+#   name         = "http-trigger"
+#   logic_app_id = azurerm_logic_app_workflow.logic_app.id
 
-  schema = <<SCHEMA
-{}
-SCHEMA
+#   schema = <<SCHEMA
+# {}
+# SCHEMA
 
-}
+# }
 
-resource "azurerm_logic_app_action_http" "example" {
-  name         = "Get KyeVault Secret"
-  logic_app_id = azurerm_logic_app_workflow.logic_app.id
-  method       = "GET"
-  uri          = "https://${azurerm_key_vault.logic_app.name}.vault.azure.net/${azurerm_key_vault_secret.mySecret.name}"
-  body = <<BODY
-  {
-                    "inputs": {
-                    "authentication": {
-                        "audience": "https://vault.azure.net",
-                        "type": "ManagedServiceIdentity"
-                    },
-                                          "queries": {
-                        "api-version": "2016-10-01"
-                    }
-                    }
-}
-BODY
-  }
+# resource "azurerm_logic_app_action_http" "example" {
+#   name         = "Get KyeVault Secret"
+#   logic_app_id = azurerm_logic_app_workflow.logic_app.id
+#   method       = "GET"
+#   uri          = "https://${azurerm_key_vault.logic_app.name}.vault.azure.net/${azurerm_key_vault_secret.mySecret.name}"
+#   body = <<BODY
+#   {
+#                     "inputs": {
+#                     "authentication": {
+#                         "audience": "https://vault.azure.net",
+#                         "type": "ManagedServiceIdentity"
+#                     },
+#                                           "queries": {
+#                         "api-version": "2016-10-01"
+#                     }
+#                     }
+# }
+# BODY
+#   }
