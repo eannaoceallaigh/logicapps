45f1ccc55651afc331f6c978316780d3733c7c35
hippogriffin <40152338+hippogriffin@users.noreply.github.com>
2022-06-30 08:49:27 +0100

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -13,27 +13,27 @@ resource "azurerm_subnet" "logic_app" {
   service_endpoints = ["Microsoft.Storage"]
 }
 
-resource "azurerm_private_endpoint" "logic_app" {
-  name                = "logic-app-endpoint"
-  location            = azurerm_resource_group.logic_app.location
-  resource_group_name = azurerm_resource_group.logic_app.name
-  subnet_id           = azurerm_subnet.logic_app.id
+# resource "azurerm_private_endpoint" "logic_app" {
+#   name                = "logic-app-endpoint"
+#   location            = azurerm_resource_group.logic_app.location
+#   resource_group_name = azurerm_resource_group.logic_app.name
+#   subnet_id           = azurerm_subnet.logic_app.id
 
-  private_service_connection {
-    name                              = "logic-app-psc"
-    is_manual_connection              = true
-    private_connection_resource_id = azurerm_storage_account.logic_app.id
-    subresource_names = ["blob"]
-    request_message                   = "PL"
-  }
+#   private_service_connection {
+#     name                              = "logic-app-psc"
+#     is_manual_connection              = true
+#     private_connection_resource_id = azurerm_storage_account.logic_app.id
+#     subresource_names = ["blob"]
+#     request_message                   = "PL"
+#   }
 
-  private_dns_zone_group {
-    name = "privatelink.blob.core.windows.net"
-    private_dns_zone_ids = [azurerm_private_dns_zone.logic_app.id]
-  }
-}
+#   private_dns_zone_group {
+#     name = "privatelink.blob.core.windows.net"
+#     private_dns_zone_ids = [azurerm_private_dns_zone.logic_app.id]
+#   }
+# }
 
-resource "azurerm_private_dns_zone" "logic_app" {
-  name                = "privatelink.blob.core.windows.net"
-  resource_group_name = azurerm_resource_group.logic_app.name
-}
+# resource "azurerm_private_dns_zone" "logic_app" {
+#   name                = "privatelink.blob.core.windows.net"
+#   resource_group_name = azurerm_resource_group.logic_app.name
+# }
