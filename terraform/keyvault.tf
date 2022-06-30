a55974879d78b16f9b4420577b341c4a8df2c020
hippogriffin <40152338+hippogriffin@users.noreply.github.com>
2022-06-30 08:52:36 +0100

    Create keyvault.tf

---
+++ terraform/keyvault.tf
@@ -0,0 +1,22 @@
+resource "azurerm_key_vault" "logic_app" {
+  name                        = "logic-app-keyvault"
+  location                    = azurerm_resource_group.logic_app.location
+  resource_group_name         = azurerm_resource_group.logic_app.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "logic_app" {
+  key_vault_id = azurerm_key_vault.logic_app.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_logic_app_workflow.logic_app.identity[0].principal_id
+
+  secret_permissions = [
+    "Get",
+    "List"
+  ]
+}
