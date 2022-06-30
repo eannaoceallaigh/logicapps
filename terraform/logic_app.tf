5e5cd7cdb3a8301b5d0d9bfa0dcdeae0c78eb9a5
hippogriffin <40152338+hippogriffin@users.noreply.github.com>
2022-06-30 09:28:57 +0100

    Update logic_app.tf

--- terraform/logic_app.tf
+++ terraform/logic_app.tf
@@ -10,3 +10,14 @@ resource "azurerm_logic_app_workflow" "logic_app" {
     type = "SystemAssigned"
   }
 }
+
+resource "azurerm_template_deployment" "logicApp" {
+  resource_group_name = azurerm_resource_group.logic_app.name
+  deployment_mode     = "Incremental"
+  name                = "logic app deployment"
+  parameters = {
+    workflows_flow_name = azurerm_logic_app_workflow.logic_app.name
+    location            = azurerm_resource_group.logic_app.location
+  }
+  template_body = data.local_file.logic_app.content
+}
